b02ecb51290ed1622d27c70d554b2e9dedc431e1
Patrick McMahon <pmcmahon14@gmail.com>
2018-01-19 21:06:46 -0800

    added math ops to page

--- PHPBeginner/FirstSteps.php
+++ PHPBeginner/FirstSteps.php
@@ -16,5 +16,24 @@ $title = "Hello World!";
 <!--use HTML element and embed PHP inside element to create page dynamically-->
 
 <h1><?php echo $title; ?></h1>
+
+<!--MATH OPERATIONS-->
+
+<?php
+
+echo 56 + 45;
+echo "<br>";
+echo 56 - 45;
+echo "<br>";
+echo 56 * 45;
+echo "<br>";
+echo 56 / 45;
+echo "<br>";
+echo "<br>";
+echo 45 + 53 + 46 / 124 - 66;
+echo "<br>";
+echo 5 + 5 * 10;
+
+?>
 </body>
 </html>
