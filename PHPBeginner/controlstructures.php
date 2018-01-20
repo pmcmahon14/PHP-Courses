d50e6163c7825ab4e949bba8488f1c4f654b8683
Patrick McMahon <pmcmahon14@gmail.com>
2018-01-19 23:47:44 -0800

    added if statement to controlstructures

--- PHPBeginner/controlstructures.php
+++ PHPBeginner/controlstructures.php
@@ -1,3 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
 <?php
 
+//IF STATEMENTS
+
+if (3 < 10){
+echo 'three is less than 10';
+} elseif (4 < 5) {
+    echo 'of course four is less than five';
+} else {
+    echo 'it is not';
+}
+
 ?>
+</body>
+</html>
