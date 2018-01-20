4a95ce4a71fd07d18822b34547b8fea44aed25b9
Patrick McMahon <pmcmahon14@gmail.com>
2018-01-19 21:29:10 -0800

    added array section to datatypes page

--- PHPBeginner/Datatypes.php
+++ PHPBeginner/Datatypes.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
 <!--MATH OPERATIONS-->
 
 <?php
@@ -16,3 +24,24 @@ echo "<br>";
 echo 5 + 5 * 10;
 
 ?>
+
+<!--ARRAYS-->
+
+<?php
+
+$numberList =
+    array(24, 37, 63, 354, 677, '5345', 345, '<h1>Hello world!</h1>');
+
+echo $numberList[0];
+
+?>
+
+<!--ASSOCIATIVE ARRAYS-->
+
+<?php
+
+
+
+?>
+</body>
+</html>
