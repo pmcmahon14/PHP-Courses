94705e693dce382afe8c0a446271ec2d5cc41c2b
Patrick McMahon <pmcmahon14@gmail.com>
2018-01-25 21:42:49 -0800

    double check

--- PHPBeginner/formdata.php
+++ PHPBeginner/formdata.php
@@ -1,7 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Patrick
- * Date: 1/25/2018
- * Time: 3:31 PM
- */
+
+if (isset($_POST['submit'])) {
+echo 'yes it works';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<form action="formdata.php" method="post">
+    <input type="text" placeholder="Enter Username">
+    <input type="password" placeholder="Enter Password">
+    <input type="submit" name="submit">
+
+</form>
+
+</body>
+</html>
