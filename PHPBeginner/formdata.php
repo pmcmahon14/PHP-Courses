bfb7efc749e9affdaf84948cfdf8fdd6b8204dad
Patrick McMahon <pmcmahon14@gmail.com>
2018-01-27 11:04:15 -0800

    added form validation to formdata and added formprocess page for validation testing

--- PHPBeginner/formdata.php
+++ PHPBeginner/formdata.php
@@ -1,7 +1,40 @@
 <?php
 
 if (isset($_POST['submit'])) {
-echo 'yes it works';
+
+    //TEST ARRAY
+    $name = array("Patrick", "Dawn", "Sierra", "Finn");
+    //TEST CONDITIONS
+    $minimum = 5;
+    $maximum = 10;
+
+//EXTRACT INFORMATION
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+echo $password;
+echo $username;
+
+//VALIDATION
+
+    if(strlen($username) < $minimum) {
+
+        echo "Username has to be longer than 5.";
+
+    }
+
+    if(strlen($username) < $maximum) {
+
+        echo "Username has to be shorter than 10.";
+
+    }
+
+    if(!in_array($username, $name)) {
+        echo "Sorry, you are not allowed to log in.";
+    } else {
+        echo "Welcome!";
+    }
+
 }
 ?>
 
@@ -14,8 +47,8 @@ echo 'yes it works';
 <body>
 
 <form action="formdata.php" method="post">
-    <input type="text" placeholder="Enter Username">
-    <input type="password" placeholder="Enter Password">
+    <input type="text" name="username" placeholder="Enter Username">
+    <input type="password" name="password" placeholder="Enter Password">
     <input type="submit" name="submit">
 
 </form>
