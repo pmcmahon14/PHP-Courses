ef873cbef8ed7465110ee303258312ee38eecfef
Patrick McMahon <pmcmahon14@gmail.com>
2018-03-31 08:22:05 -0700

    cleared up appearance on Datatypes.php

--- PHPBeginner/Datatypes.php
+++ PHPBeginner/Datatypes.php
@@ -22,6 +22,7 @@ echo "<br>";
 echo 45 + 53 + 46 / 124 - 66;
 echo "<br>";
 echo 5 + 5 * 10;
+echo "<br>"
 
 ?>
 
@@ -29,16 +30,22 @@ echo 5 + 5 * 10;
 
 <?php
 
+echo "<h3>Arrays</h3>";
+
 $numberList =
     array(24, 37, 63, 354, 677, '5345', 345, '<h1>Hello world!</h1>');
 
 echo $numberList[0];
+
+echo "<br>"
 
 ?>
 
 <!--ASSOCIATIVE ARRAYS-->
 
 <?php
+
+echo "<h3>Associative Arrays</h3>";
 
 $number = array('Dawn', 'Finn', 'Sierra');
 
---
+++ PHPBeginner/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Patrick
+ * Date: 1/27/2018
+ * Time: 1:18 PM
+ */
